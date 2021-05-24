d509fcc0dfbc0fc41e9d31fe1559857bc1efd1db
James Paul Sampedro <jamez.8833@gmail.com>
2021-05-24 19:10:10 +0800

    Add project files.

---
+++ cash_advance_salary_loan/User/Profile.vb
@@ -0,0 +1,13 @@
+﻿Public Class frm_profile
+    Private Sub frm_profile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Cash_advance_salary_loanDataSet.cash_advance' table. You can move, or remove it, as needed.
+        Me.Cash_advanceTableAdapter.Fill(Me.Cash_advance_salary_loanDataSet.cash_advance)
+        'TODO: This line of code loads data into the 'Cash_advance_salary_loanDataSet.salary_loan' table. You can move, or remove it, as needed.
+        Me.Salary_loanTableAdapter.Fill(Me.Cash_advance_salary_loanDataSet.salary_loan)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_SLsortLatest.Click
+
+    End Sub
+End Class
